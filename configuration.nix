d698b9ac5c9b0ca162beff3263fee413c29a86c8
mdnlss <rob73hall@gmail.com>
2025-02-22 14:14:32 -0500

    add systemdboot

--- configuration.nix
+++ configuration.nix
@@ -12,18 +12,19 @@
   ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   boot.loader.efi.efiSysMountPoint = "/boot/efi"; # for dual booting 2 Linux Distros
-  boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    #splashImage = ./assets/lain.png;
-    splashImage = ./assets/kami.png;
-    gfxmodeBios = "2880x1920";
-    device = "/dev/disk/by-partlabel/disk-main-boot";
-    version = 2;
-    useOSProber = true;
-    enable = true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
+  boot.loader.systemd-boot.enable = true;
+  # boot.loader.grub = {
+  #   # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+  #   #splashImage = ./assets/lain.png;
+  #   splashImage = ./assets/kami.png;
+  #   gfxmodeBios = "2880x1920";
+  #   device = "/dev/disk/by-partlabel/disk-main-boot";
+  #   version = 2;
+  #   useOSProber = true;
+  #   enable = true;
+  #   efiSupport = true;
+  #   efiInstallAsRemovable = true;
+  # };
 
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
