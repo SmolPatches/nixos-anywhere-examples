904fae5be3f803e8608d4ea92155bb05356b7968
Robert <102612257+SmolPatches@users.noreply.github.com>
2025-02-25 11:50:50 +0100

    feat: make user info common

--- modules/common.nix
+++ modules/common.nix
@@ -7,7 +7,16 @@
     allowUnfree = true;
     allowBroken = false;
   };
-  #{ config, pkgs, lib }: {
+  user.users.lain = {
+    # add HashedPassword( in sops for even more security )
+    initialHashedPassword = "$6$0o1YuLY57h96oMrF$KSt/VqRC9Nz4nbz5L03HaeJu5UucCNyBo/OiQYiaOcBhU/PLqW4/wD4xJAP32Oc45sASDT.DigNDYzU8meKTZ/";
+    extraGroups = [
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    openssh.authorizedKeys.keyFiles = let ssh_keys = (builtins.fetchurl { url = "https://github.com/SmolPatches.keys"; sha256 = "19x3p8ddrnvcsr7bfqnzbwn717crqmhv2q194ib70vbjb5y3nbxi"; }); in [ ssh_keys ]; # point key files to the thing in nix_store
+  };
   security.sudo = {
     enable = true;
     execWheelOnly = true;
--- modules/laptop.nix
+++ modules/laptop.nix
@@ -36,16 +36,8 @@
   };
   users = {
     #mutableUsers = lib.mkForce false;
-    mutableUsers = true;
+    mutableUsers = lib.mkForce true; # common has this set to false
     users.lain = {
-      # add HashedPassword( in sops for even more security )
-      initialHashedPassword = "$6$0o1YuLY57h96oMrF$KSt/VqRC9Nz4nbz5L03HaeJu5UucCNyBo/OiQYiaOcBhU/PLqW4/wD4xJAP32Oc45sASDT.DigNDYzU8meKTZ/";
-      extraGroups = [
-        "wheel"
-      ];
-      shell = pkgs.zsh;
-      isNormalUser = true;
-      openssh.authorizedKeys.keyFiles = let ssh_keys = (builtins.fetchurl { url = "https://github.com/SmolPatches.keys"; sha256 = "19x3p8ddrnvcsr7bfqnzbwn717crqmhv2q194ib70vbjb5y3nbxi"; }); in [ ssh_keys ]; # point key files to the thing in nix_store
       packages = with pkgs; [
         obsidian
         zip
@@ -102,6 +94,7 @@
           vi_mode = true;
         };
       };
+      services.kubo.enable = lib.mkForce false; # this is the one machine i dont want to run kubo
     };
     power-profiles-daemon.enable = lib.mkForce false; # this conflicts w/ tlp
     tlp.enable = true;
